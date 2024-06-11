bb0f5f2e3afef79c4455be45759796923073fb46
Jaikrishna7271 <jayakrishna7271@gmail.com>
2024-06-11 09:28:21 +0530

    pipelineNODE

---
+++ cicd/main.tf
@@ -0,0 +1,56 @@
+module "jenkins" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "jenkins-tf"
+
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-0a7f25def76c5aa35"] #replace your SG
+  subnet_id = "subnet-04e9ac8cfc6f98d81" #replace your Subnet
+  ami = data.aws_ami.ami_info.id
+  user_data = file("jenkins.sh")
+  tags = {
+    Name = "jenkins-tf"
+  }
+}
+
+module "jenkins_agent" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "jenkins-agent"
+
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-0a7f25def76c5aa35"]
+  # convert StringList to list and get first element
+  subnet_id = "subnet-04e9ac8cfc6f98d81"
+  ami = data.aws_ami.ami_info.id
+  user_data = file("jenkins-agent.sh")
+  tags = {
+    Name = "jenkins-agent"
+  }
+}
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
+
+  zone_name = var.zone_name
+
+  records = [
+    {
+      name    = "jenkins"
+      type    = "A"
+      ttl     = 1
+      records = [
+        module.jenkins.public_ip
+      ]
+    },
+    {
+      name    = "jenkins-agent"
+      type    = "A"
+      ttl     = 1
+      records = [
+        module.jenkins_agent.private_ip
+      ]
+    }
+  ]
+
+}
