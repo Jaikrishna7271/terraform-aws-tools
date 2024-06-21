269c1c2504a7157c5f949f3f199801e7b679ab82
Jaikrishna7271 <jayakrishna7271@gmail.com>
2024-06-21 10:43:06 +0530

    jenkins file1

--- cicd/main.tf
+++ cicd/main.tf
@@ -28,6 +28,21 @@ module "jenkins_agent" {
     Name = "jenkins-agent"
   }
 }
+module "nexus" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "nexus"
+
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = ["sg-0a7f25def76c5aa35"]
+  # convert StringList to list and get first element
+  subnet_id = "subnet-04e9ac8cfc6f98d81"
+  ami = data.aws_ami.nexus_ami_info.id
+  tags = {
+    Name = "nexus"
+  }
+}
+
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
@@ -42,6 +57,7 @@ module "records" {
       records = [
         module.jenkins.public_ip
       ]
+      allow_overwrite = true
     },
     {
       name    = "jenkins-agent"
@@ -50,6 +66,17 @@ module "records" {
       records = [
         module.jenkins_agent.private_ip
       ]
+      allow_overwrite = true
+    },
+    {
+      name    = "nexus"
+      type    = "A"
+      ttl     = 1
+      allow_overwrite = true
+      records = [
+        module.nexus.private_ip
+      ]
+      allow_overwrite = true
     }
   ]
 
